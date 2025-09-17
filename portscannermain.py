b583af5f8e9cbb868c4e9492059ad07b818a39fe
Patrick <pmlester3@icloud.com>
2025-09-16 21:08:23 -0500

    Changed to scan common ports, added commits

--- portscannermain.py
+++ portscannermain.py
@@ -3,10 +3,10 @@
 #Only used to scan my own machine
 
 import socket
-
+"""
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4, TCP
 sock.settimeout(1) 
-result = sock.connect_ex(("127.0.0.1", 80))
+result = sock.connect_ex(("127.0.0.1", 80)) #Local host, checking port 80
 sock.close()
 
 #--------------------- Code 1 ----------------------
@@ -17,5 +17,25 @@ if result == 0:
     print("Port 80 is open")
 else:
     print("Port 80 is closed")
+                                        """
 
+
+def port_check(host, port):
+    """Function to check openness on a port"""
     
+    #AF_INET signals to use IPv4 addresses
+    #SOCK_STREAM signals to use TCP protocol
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)#Gives up after one second
+    result = sock.connect_ex((host, port)) #connect_ex is what is trying to connect to host and port
+    sock.close #closes the socket
+
+    return result == 0
+target = "127.0.0.1" #This is localhost, using other IP's is illegal
+common_ports = [22, 80, 443, 3389, 53, 25] #SSH, HTTP, HTTPS, RDP, DNS, SMTP
+
+for port in common_ports:
+    if port_check(target, port): #call the function to get the port with the common ports
+        print(f"Port {port} is OPEN")
+    else:
+        print(f"Port {port} is CLOSED.")
